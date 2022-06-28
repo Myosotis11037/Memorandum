eee9fab46a6db922d47deb654d9ad6ef3be096ed
Myosotis11037 <GiT./114hUB810>
2022-06-28 10:00:39 +0800

    initialization

---
+++ app/src/main/java/com/linya/memorandum/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.linya.memorandum
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_layout)
+        this.title = "我的备忘录"
+
+        findViewById<Button>(R.id.login_btn).setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?){
+        when(v?.id){
+            R.id.login_btn -> {
+                val username = findViewById<EditText>(R.id.loginAccount).text
+                val password = findViewById<EditText>(R.id.loginPassword).text
+
+                val okHttpClient = OkHttpClient()
+                val url = "http://10.83.130.5:8080/v1/android/sign"
+                val request = Request.Builder().url(url)
+            }
+        }
+    }
+}
