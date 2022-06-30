41714d69419d631524ee6a89494cc47efc11ef45
Myosotis11037 <GiT./114hUB810>
2022-06-30 14:18:16 +0800

    完成了新建备忘录功能

---
+++ app/src/main/java/com/linya/memorandum/WriteNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.linya.memorandum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WriteNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_note)
+    }
+}
