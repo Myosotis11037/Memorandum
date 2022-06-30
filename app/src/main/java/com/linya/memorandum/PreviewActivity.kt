08aa7ade7f47f0f0a46c5671678984804bc378a9
Myosotis11037 <GiT./114hUB810>
2022-06-30 23:41:22 +0800

    完成了预览的start

---
+++ app/src/main/java/com/linya/memorandum/PreviewActivity.kt
@@ -0,0 +1,11 @@
+package com.linya.memorandum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
