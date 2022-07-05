c72c84308bd31342fdff23fffe6b7f9e588b42fe
Myosotis11037 <GiT./114hUB810>
2022-07-05 10:31:28 +0800

    修复了备忘录标题、类别或内容可以为空的bug

--- app/src/main/java/com/linya/memorandum/WriteNoteActivity.kt
+++ app/src/main/java/com/linya/memorandum/WriteNoteActivity.kt
@@ -6,6 +6,7 @@ import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.linya.memorandum.db.SQLiteDB
 import com.linya.memorandum.entity.NoteBean
@@ -13,8 +14,8 @@ import kotlinx.android.synthetic.main.activity_write_note.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class WriteNoteActivity : AppCompatActivity() , View.OnClickListener{
-    private var mSQLiteDB : SQLiteDB ?= null
+class WriteNoteActivity : AppCompatActivity(), View.OnClickListener {
+    private var mSQLiteDB: SQLiteDB? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,28 +29,41 @@ class WriteNoteActivity : AppCompatActivity() , View.OnClickListener{
     override fun onClick(v: View?) {
 
         val title = edit_title?.text.toString()
+        val type = edit_type?.text.toString()
         val content = edit_content?.text.toString()
         val date = Date()
         val addTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date)
         val updateTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date)
-        val type = edit_type?.text.toString()
         val note_id = date.time.toString()
 
-        val noteBean = NoteBean()
-        noteBean.note_id = note_id
-        noteBean.type = type
-        noteBean.title = title
-        noteBean.content = content
-        noteBean.create_time = addTime
-        noteBean.update_time = updateTime
-        mSQLiteDB = SQLiteDB.getInstance(this)
+        if (title == "" || type == "" || content == "") {
+            AlertDialog.Builder(this).apply {
+                setTitle("Error")
+                setMessage("标题、类别或备忘录内容均不能为空！！")
+                setCancelable(true)
+                setPositiveButton("OK") { _, _ ->
+                }
+                setNegativeButton("Cancel") { _, _ ->
+                }
+                show()
+            }
+        } else {
+            val noteBean = NoteBean()
+            noteBean.note_id = note_id
+            noteBean.type = type
+            noteBean.title = title
+            noteBean.content = content
+            noteBean.create_time = addTime
+            noteBean.update_time = updateTime
+            mSQLiteDB = SQLiteDB.getInstance(this)
 
-        if(mSQLiteDB?.addNote(noteBean)!!){
-            Toast.makeText(this,"保存成功！",Toast.LENGTH_SHORT).show()
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        }else{
-            Toast.makeText(this, "保存失败！",Toast.LENGTH_SHORT).show()
+            if (mSQLiteDB?.addNote(noteBean)!!) {
+                Toast.makeText(this, "保存成功！", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "保存失败！", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
